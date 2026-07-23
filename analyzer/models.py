ac718baa62e9c2305d55db1de3ca0fd1086a5967
ArtinArenas <artinarenas@gmail.com>
2026-07-23 15:28:22 -0300

    [feat] agregar parseo y estructura iniciales de eventos

---
+++ analyzer/models.py
@@ -0,0 +1,10 @@
+
+
+class Evento:
+    def __init__(self, timestamp, hour, action, user_id, ip_address):
+        self.timestamp = timestamp      # Atributo de instancia
+        self.hour = hour                # Atributo de instancia
+        self.action = action            # Atributo de instancia
+        self.user_id = user_id          # Atributo de instancia
+        self.ip_address = ip_address    # Atributo de instancia
+        
---
+++ analyzer/parser.py
@@ -0,0 +1,27 @@
+from models import Evento
+import re 
+from dataclasses import dataclass
+
+
+def parse_log():
+
+    registros = []
+
+    patron = re.compile(
+        r'(?P<timestamp>\d{4}-\d{2}-\d{2})\s+'
+        r'(?P<hour>\d{6})\s+'
+        r'(?P<action>\w+)\s+'
+        r'user=(?P<user_id>\w+)\s+'
+        r'ip=(?P<ip_address>\d+\.\d+\.\d+\.\d+)'
+    )
+
+    #Lectura del log linea a linea
+    with open("activity.log", "r") as archivo:
+        for linea in archivo:
+            m = re.search(patron, linea)
+            if m:
+                # Agrego un registro de tipo Evento con los datos extraídos
+                registros.append(Evento(m.group("timestamp"), m.group("hour"), m.group("action"), m.group("user_id"), m.group("ip_address")))
+    
+    
+    return registros
