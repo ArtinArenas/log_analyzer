6c8f9dde2adb02f5ba4d222c9dc5854cfe7169e7
ArtinArenas <artinarenas@gmail.com>
2026-08-01 11:55:31 -0300

    Se agrega el objeto Alert y se agregan propiedades a Event

--- analyzer/models.py
+++ analyzer/models.py
@@ -1,21 +1,22 @@
-
 # Objeto donde se almacenan los registros obtenidos de los logs
 class Event:
-    def __init__(self, timestamp, hour, action, user_id, ip_address):
-        self.timestamp = timestamp      # Atributo de instancia
-        self.hour = hour                # Atributo de instancia
-        self.action = action            # Atributo de instancia
-        self.user_id = user_id          # Atributo de instancia
-        self.ip_address = ip_address    # Atributo de instancia
-        
+    def __init__(self, timestamp, hour, action, user_id, ip_address, attempts):
+        self.timestamp = timestamp
+        self.hour = hour
+        self.action = action
+        self.user_id = [user_id] if not isinstance(user_id, list) else user_id
+        self.ip_address = ip_address
+        self.attempts = attempts
+
 
 # Objeto que retornan los detectores
 class Alert:
-    def __init__(self, timestamp, hour, severity, detector, messaje, user_id, ip_address):
-        self.timestamp = timestamp      # Atributo de instancia
-        self.hour = hour                # Atributo de instancia
-        self.severity = severity        # Atributo de instancia
-        self.detector = detector        # Atributo de instancia
-        self.messaje = messaje          # Atributo de instancia
-        self.user_id = user_id          # Atributo de instancia
-        self.ip_address = ip_address    # Atributo de instancia
+    def __init__(self, timestamp, hour, severity, detector, message, user_id, ip_address, attempts=0):
+        self.timestamp = timestamp
+        self.hour = hour
+        self.severity = severity
+        self.detector = detector
+        self.message = message
+        self.user_id = user_id
+        self.ip_address = ip_address
+        self.attempts = attempts
