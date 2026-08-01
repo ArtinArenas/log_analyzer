152d0147a216acd1db2163d13b41b9efa41b35fd
ArtinArenas <artinarenas@gmail.com>
2026-08-01 12:05:10 -0300

    Se agregan tests

--- analyzer/main.py
+++ analyzer/main.py
@@ -1,26 +1,9 @@
 from parser import parse_log
-import models
-from detector import * #attemptsForIp, failedAttemptsForIp
+from detector import attempts_for_ip
+from reports import imp_report
 
-registros = parse_log()
-for registro in registros:
-    print(f"Timestamp: {registro.timestamp}, Hour: {registro.hour}, Action: {registro.action}, User ID: {registro.user_id}, IP Address: {registro.ip_address}")
+records = parse_log()
+attempts = attempts_for_ip(records)
 
-attempts = attemptsForIp(registros)
-failed = failedAttemptsForIp(registros)
-#brute_force = bruteForceForIp(registros)
-brute_force = bruteForceForIp(registros, 3)
-#suspicious_hours = suspiciousActivityByHour(registros, "002200", "060000")
-suspicious_hours = suspiciousActivityByHour(registros, "000000", "050000")
-#public_ips = publicIps(registros)
-
-print(attempts)
-
-print("\n\nIntentos fallidos por IP:")
-print(failed)
-
-print("\n\nFuerza bruta por IP:")
-print(brute_force)
-
-print("\n\nActividad sospechosa por hora:")
-print(suspicious_hours)
+print("\n\nIntentos: ")
+imp_report(attempts)
